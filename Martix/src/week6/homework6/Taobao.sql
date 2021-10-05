674fd2711c04a3bf0f358f8117dbd849127c7d77
nongxi <18598713+nongxi@users.noreply.github.com>
2021-10-05 03:33:59 -0700

    Initial commit

--- Martix/src/week6/homework6/Taobao.sql
+++ Martix/src/week6/homework6/Taobao.sql
@@ -1,5 +1,5 @@
 CREATE TABLE TUser(
-    UserNumber int (8) NOT NULL,
+    UserNumber int (8) ,
     Nickname VARCHAR(10),
     MobileNumber int (13) NOT NULL,
     Email VARCHAR(20) NOT NULL,
@@ -9,17 +9,20 @@ CREATE TABLE TUser(
     PRIMARY KEY(UserNumber)
 );
 
+INSERT INTO TUser VALUES(20093200,'Java1','18826221779','java@163.com',,'FALSE');
+-- issue
 CREATE TABLE TProduct(
-    ProductNumber int(8) NOT NULL,
+    ProductNumber int(8) ,
     ProductName VARCHAR(20) NOT NULL,
-    ProductSize VARCHAR(20),
+    ProductSize VARCHAR(20) NOT NULL,
     InputStoreHouseTime TIMESTAMP  NOT NULL,
     OutputStoreHouseTime TIMESTAMP  NOT NULL,
-    ProductTime CURRENT_DATE NOT NULL,
-    ProductCarCode int(13) NOT NULL,
-    ProductSourceCompany VARCHAR(20) NOT NULL
+    ProductTime CURRENT_DATE,
+    ProductCarCode int(13),
+    ProductSourceCompany VARCHAR(20)
 );
 
+-- issue
 CREATE TABLE TOrder(
     OrderNumber int(20) NOT NULL,
     BuyerName VARCHAR(20) NOT NULL, --NickName

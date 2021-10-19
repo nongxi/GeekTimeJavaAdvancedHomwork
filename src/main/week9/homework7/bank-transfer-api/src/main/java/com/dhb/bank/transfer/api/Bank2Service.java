5b8df72a6cc34c1b4ef28c82eadc689af3370098
Martix <Ironxss@protonmail.com>
2021-10-19 03:39:58 -0700

    week10:Geektime,homework10

--- src/main/week9/homework7/bank-transfer-api/src/main/java/com/dhb/bank/transfer/api/Bank2Service.java
+++
@@ -1,9 +0,0 @@
-package com.dhb.bank.transfer.api;
-
-import org.dromara.hmily.annotation.Hmily;
-
-public interface Bank2Service {
-
-	@Hmily
-	Boolean transfer(String tid,int customerId,int amount);
-}
--- src/main/week9/homework7/bank-transfer-api/src/main/java/com/dhb/bank/transfer/api/BankAccountService.java
+++
@@ -1,9 +0,0 @@
-package com.dhb.bank.transfer.api;
-
-public interface BankAccountService {
-
-
-	void subtractAccountBalance(String tid,int customerId, int amount);
-
-	void addAccountBalance(String tid,int customerId, int amount);
-}
